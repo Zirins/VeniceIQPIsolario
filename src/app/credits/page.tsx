51b842c0de72a65729dfbbc1a24cdbafa78ecca5
Zirins <BensonLin80169@gmail.com>
2025-05-22 13:43:16 +0200

    Added data directory and credits members

--- src/app/credits/page.tsx
+++ src/app/credits/page.tsx
@@ -1,4 +1,27 @@
 export default function Credits() {
+  // const sections = [
+  //     {
+  //         title: 'Bridges',
+  //         names: 'Bella Battaglia, Emma Herman, Griffin St. Pierre, & Luca Villano',
+  //     },
+  //     {
+  //         title: 'Canals',
+  //         names: 'Dylan Leung, Jack Ostrander, & Salvator Sapienza'
+  //     },
+  //     {
+  //         title: 'Docks',
+  //         names: 'Jiahui Lin, Diego McManus, Andrew Medvetsky, & Taeha Song'
+  //     },
+  //     {
+  //         title: 'Isles',
+  //         names: 'Nathan Cote, Alessandra Giovenco, Benson Lin, & Arianna Xie'
+  //     },
+  //     {
+  //         title: 'Streets',
+  //         names: 'Travis Burke, Tommy Reardon, Gleb Vlasov, & Brody Watson'
+  //     }
+  //     ]
+
   return (
     <div>
       <p className="text-2xl">hello</p>
