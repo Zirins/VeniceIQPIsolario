8ac46d6561954bbed1822aa7e6c12684ffa262cb
Zirins <BensonLin80169@gmail.com>
2025-05-26 13:46:37 +0200

    Added query by Island ID and wrapped layout in Apollo

--- src/lib/graphql/queries.ts
+++ src/lib/graphql/queries.ts
@@ -1,12 +1,35 @@
+// src/lib/graphql/queries.ts
 import { gql } from '@apollo/client'
 
-export const GET_ALL_ISLANDS = gql`
-  query GetAllIslands {
+export const GET_ISLANDS = gql`
+  query GetIslands {
     islands {
       id
       name
-      description
-      imageUrl
+      population
+      congestion
+      accessibility
+      affordability
+      airbnbCount
+      geometry {
+        type
+        coordinates
+      }
+    }
+  }
+`
+
+export const GET_ISLAND_BY_ID = gql`
+  query GetIslandById($id: ID!) {
+    island(id: $id) {
+      id
+      name
+      population
+      indicators {
+        congestion
+        accessibility
+        affordability
+      }
     }
   }
 `
