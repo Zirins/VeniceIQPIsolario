15e9f79a16894069cd464984974acdf5c8dfd000
Zirins <BensonLin80169@gmail.com>
2025-05-27 22:48:57 +0200

    Updated island context to use fake data and added fake data onto leaflet

--- src/components/map/InteractiveMap.tsx
+++ src/components/map/InteractiveMap.tsx
@@ -1,15 +1,33 @@
 'use client'
 
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet'
+import { ISLANDS } from '@/data/islands'
+import { useIsland } from '@/context/IslandContext'
 import 'leaflet/dist/leaflet.css'
 
 export default function InteractiveMap() {
+  const { selectIsland } = useIsland()
+
   return (
     <MapContainer center={[45.4379, 12.3346]} zoom={13} style={{ height: '100vh', width: '100%' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="&copy; OpenStreetMap contributors"
       />
+
+      {ISLANDS.map((island) => (
+        <GeoJSON
+          key={island.id}
+          data={{
+            type: 'Feature',
+            properties: { id: island.id },
+            geometry: island.geometry!,
+          }}
+          eventHandlers={{
+            click: () => selectIsland(island.id),
+          }}
+        />
+      ))}
     </MapContainer>
   )
 }
